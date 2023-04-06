5ca11b3290c9821dea4af78e6706080a8e72475a
araqelyanani <araqelyanani2016@yandex.ru>
2023-04-06 23:17:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import cv2
+import time
+
+# 1 пункт
+bgr_img = cv2.imread('variant-3.jpeg')
+
+hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+cv2.imwrite('hsv_image.jpeg', hsv_img)
+
+
+cv2.imshow("Original Image", bgr_img)
+cv2.imshow('HSV image', hsv_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# 2, 3 пункты + допзадание
+def video_processing():
+    cap = cv2.VideoCapture('video.mp4')
+    down_points = (640, 480)
+    i = 0
+    img = cv2.imread('img_1.png')
+    img_height, img_width, _ = img.shape
+    color_red = (0, 0, 255)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.resize(frame, down_points, interpolation=cv2.INTER_LINEAR)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
+
+        cv2.rectangle(frame, (220, 140), (440, 340), color_red, thickness=2, lineType=8, shift=0)  # квадрат 200х200
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) > 0:
+            c = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            if i % 5 == 0:
+                a = x + (w // 2)
+                b = y + (h // 2)
+                # print(a, b)
+
+            if x >= 220 and y >= 140 and x+w <= 440 and y+h <= 340: # проверка на попадание
+                print(True)
+            else:
+                print(False)
+
+            frame[y:y+img_height , x:x+img_width] = img # "добавляем муху"
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        time.sleep(0.1)
+        i += 1
+
+    cap.release()
+
+if __name__ == '__main__':
+    video_processing()
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
